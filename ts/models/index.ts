dab4ae9a5bbd9674a6fb4ac246321047db340340
touy <touya.ra@gmail.com>
2021-08-29 23:15:43 +0700

    first push

---
+++ ts/models/index.ts
@@ -0,0 +1,21 @@
+import * as sequelize from "sequelize";
+import {VDPFactory} from './vdp.entity';
+
+export const dbConnection = new sequelize.Sequelize(
+    (process.env.DB_NAME = "test"),
+    (process.env.DB_USER = "admin"),
+    (process.env.DB_PASSWORD = "123456"),
+    {
+        port: Number(process.env.DB_PORT) || 5433,
+        host: process.env.DB_HOST || "localhost",
+        dialect: "postgres",
+        pool: {
+            min: 0,
+            max: 5,
+            acquire: 30000,
+            idle: 10000,
+        },
+    }
+);
+export const VDPEntity =  VDPFactory('VDP',dbConnection);
+VDPEntity.sync({force:true});
