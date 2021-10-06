a51e0beffe4bc45d9a64c4d0eaed10018704ef9d
touy <touya.ra@gmail.com>
2021-10-06 20:40:54 +0700

    fixing body undefine

--- version2/back/source/server.ts
+++ version2/back/source/server.ts
@@ -8,8 +8,8 @@ import path from 'path';
 
 export let app: Application = api;
 
-app.use(express.json({ limit: '500mb' }));
-app.use(express.urlencoded({ limit: '500mb', extended: true, parameterLimit: 50000 }));
+app.use(express.json());
+app.use(express.urlencoded({  extended: true }));
 
 app.use(cors());
 app.use(cookieParser());
