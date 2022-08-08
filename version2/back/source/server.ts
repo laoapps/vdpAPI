ac4a6963e34b09aea1f960e5dff9f03887a6fda5
touy <touya.ra@gmail.com>
2022-08-08 16:45:47 +0700

    update

--- version2/back/source/server.ts
+++ version2/back/source/server.ts
@@ -3,31 +3,36 @@ import express from 'express';
 import cors from 'cors';
 import * as bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import app from './api/api';
+
 import helmet from 'helmet';
-
+import path from 'path';
+import { API } from './api/api';
 // export let app: Application = api;
-
+const app: Application = express();
+// app.options('*',cors())
 app.use(cors());
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb', extended: true, parameterLimit: 50000 }));
 app.use(cookieParser());
 app.disable('x-powered-by');
 app.use(helmet.hidePoweredBy());
-app.use(function(req, res, next) {
+app.use('/static', express.static(path.join(__dirname, 'img')));
+// app.use(function(req, res, next) {
     
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    // res.setHeader('Access-Control-Allow-Credentials', true);
-   next();
-  });
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     // res.setHeader('Access-Control-Allow-Credentials', true);
+//    next();
+//   });
 // <==  my API ==>
 // app.use('/api', api)
 
@@ -45,6 +50,7 @@ app.get('/api/v1', (req: express.Request, res: express.Response) => {
     let post = app._router.stack.filter((r: any) => r.route && r.route.methods.post).map((r: any) => r.route.path);
     res.send({ get: get, post: post });
 });
+new API(app);
 let PORT = 23000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log('Server is running on PORT:' + PORT);
