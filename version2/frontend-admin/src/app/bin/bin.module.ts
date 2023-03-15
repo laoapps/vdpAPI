a2e162eb60c55c10994701732cc1a9b886d513f6
binrydara <binrydara@gmail.com>
2023-03-15 17:02:15 +0700

    new

---
+++ version2/frontend-admin/src/app/bin/bin.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BinRoutingModule } from './bin-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BinRoutingModule
+  ]
+})
+export class BinModule { }
